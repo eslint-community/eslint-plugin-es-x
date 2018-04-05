fe333e309a92dffc26f71db40cf7c53f9b08c224
ota <otameshiyo23@gmail.com>
2018-04-05 11:32:39 +0900

    Update: make `es/no-unicode-codepoint-escapes` fixable (#1)

--- docs/rules/no-unicode-codepoint-escapes.md
+++ docs/rules/no-unicode-codepoint-escapes.md
@@ -2,6 +2,8 @@
 
 This rule reports ES2015 Unicode code point escape sequences as errors.
 
+- :wrench: The `--fix` option on the [command line](http://eslint.org/docs/user-guide/command-line-interface#fix) can automatically fix some of the problems reported by this rule.
+
 ## Examples
 
 ⛔ Examples of **incorrect** code for this rule:
@@ -9,4 +11,11 @@ This rule reports ES2015 Unicode code point escape sequences as errors.
 ```js
 const a\u{31} = `foo`
 const a2 = "a\u{62}b"
+```
+
+👌 Examples of **correct** code for this rule:
+
+```js
+const a\u0031 = `foo`
+const a2 = "a\u0062b"
 ```
--- tests/lib/rules/no-unicode-codepoint-escapes.js
+++ tests/lib/rules/no-unicode-codepoint-escapes.js
@@ -25,36 +25,42 @@ new RuleTester().run("no-unicode-codepoint-escapes", rule, {
     invalid: [
         {
             code: "\\u{45} = 1",
+            output: "\\u0045 = 1",
             errors: [
                 "ES2015 Unicode code point escape sequences are forbidden.",
             ],
         },
         {
             code: "a\\u{45}b = 1",
+            output: "a\\u0045b = 1",
             errors: [
                 "ES2015 Unicode code point escape sequences are forbidden.",
             ],
         },
         {
             code: "'\\u{45}'",
+            output: "'\\u0045'",
             errors: [
                 "ES2015 Unicode code point escape sequences are forbidden.",
             ],
         },
         {
             code: "'a\\u{45}b'",
+            output: "'a\\u0045b'",
             errors: [
                 "ES2015 Unicode code point escape sequences are forbidden.",
             ],
         },
         {
             code: "`\\u{45}`",
+            output: "`\\u0045`",
             errors: [
                 "ES2015 Unicode code point escape sequences are forbidden.",
             ],
         },
         {
             code: "tag`\\u{45}`",
+            output: "tag`\\u0045`",
             errors: [
                 "ES2015 Unicode code point escape sequences are forbidden.",
             ],
@@ -62,22 +68,93 @@ new RuleTester().run("no-unicode-codepoint-escapes", rule, {
         {
             //eslint-disable-next-line no-template-curly-in-string
             code: "`\\u{45}${a}\\u{46}`",
+            //eslint-disable-next-line no-template-curly-in-string
+            output: "`\\u0045${a}\\u0046`",
             errors: [
+                "ES2015 Unicode code point escape sequences are forbidden.",
                 "ES2015 Unicode code point escape sequences are forbidden.",
             ],
         },
         {
             //eslint-disable-next-line no-template-curly-in-string
             code: "tag`\\u{45}${a}\\u{46}`",
+            //eslint-disable-next-line no-template-curly-in-string
+            output: "tag`\\u0045${a}\\u0046`",
             errors: [
+                "ES2015 Unicode code point escape sequences are forbidden.",
                 "ES2015 Unicode code point escape sequences are forbidden.",
             ],
         },
         {
             //eslint-disable-next-line no-template-curly-in-string
             code: "tag`\\u{XXXZX}${a}\\u{46}`",
+            //eslint-disable-next-line no-template-curly-in-string
+            output: "tag`\\u{XXXZX}${a}\\u0046`",
             errors: [
                 "ES2015 Unicode code point escape sequences are forbidden.",
+            ],
+        },
+        {
+            code: '"\\u{20BB7}"',
+            output: '"\\uD842\\uDFB7"',
+            errors: [
+                {
+                    message:
+                        "ES2015 Unicode code point escape sequences are forbidden.",
+                    column: 2,
+                    line: 1,
+                },
+            ],
+        },
+        {
+            code: "`\\u{20BB7}`",
+            output: "`\\uD842\\uDFB7`",
+            errors: [
+                {
+                    message:
+                        "ES2015 Unicode code point escape sequences are forbidden.",
+                    column: 2,
+                    line: 1,
+                },
+            ],
+        },
+        {
+            code: `
+a=\`\${a}\\u{D842}\\u{DFB7}\`
+b="\\u{20BB7}"
+`,
+            output: `
+a=\`\${a}\\uD842\\uDFB7\`
+b="\\uD842\\uDFB7"
+`,
+            errors: [
+                {
+                    message:
+                        "ES2015 Unicode code point escape sequences are forbidden.",
+                    line: 2,
+                    column: 8,
+                    nodeType: "TemplateElement",
+                    endLine: 2,
+                    endColumn: 16,
+                },
+                {
+                    message:
+                        "ES2015 Unicode code point escape sequences are forbidden.",
+                    line: 2,
+                    column: 16,
+                    nodeType: "TemplateElement",
+                    endLine: 2,
+                    endColumn: 24,
+                },
+                {
+                    message:
+                        "ES2015 Unicode code point escape sequences are forbidden.",
+                    line: 3,
+                    column: 4,
+                    nodeType: "Literal",
+                    endLine: 3,
+                    endColumn: 13,
+                },
             ],
         },
     ],
