4f6f571efdbd2f4f586dcb908a4da4aac11a57f3
ota <otameshiyo23@gmail.com>
2018-05-06 17:42:19 +0900

    Update: Make `es/no-template-literals` fixable (#5)

--- docs/rules/no-template-literals.md
+++ docs/rules/no-template-literals.md
@@ -2,13 +2,23 @@
 
 This rule reports ES2015 template literals as errors.
 
+- :wrench: The `--fix` option on the [command line](http://eslint.org/docs/user-guide/command-line-interface#fix) can automatically fix some of the problems reported by this rule.
+
 ## Examples
 
 ⛔ Examples of **incorrect** code for this rule:
 
 ```js
 const a1 = `foo`
-const a2 = tag`foo`
+const a2 = `foo${bar}baz`
+const a3 = tag`foo`
+```
+
+👌 Examples of **correct** code for this rule:
+
+```js
+const a1 = "foo"
+const a2 = "foo"+bar+"baz"
 ```
 
 ## 📚 References
--- tests/lib/rules/no-template-literals.js
+++ tests/lib/rules/no-template-literals.js
@@ -8,19 +8,80 @@ const RuleTester = require("../../tester")
 const rule = require("../../../lib/rules/no-template-literals.js")
 
 new RuleTester().run("no-template-literals", rule, {
-    valid: ["'foo'", '"bar"'],
+    valid: [
+        "'foo'",
+        '"bar"',
+        `
+const a1 = "foo"
+const a2 = "foo"+bar+"baz"
+    `,
+    ],
     invalid: [
         {
             code: "`foo`",
+            output: `"foo"`,
             errors: ["ES2015 template literals are forbidden."],
         },
         {
             code: "tag`foo`",
+            output: null,
             errors: ["ES2015 template literals are forbidden."],
         },
         {
             //eslint-disable-next-line no-template-curly-in-string
             code: "`foo${a}bar${b}baz`",
+            output: `"foo"+a+"bar"+b+"baz"`,
+            errors: ["ES2015 template literals are forbidden."],
+        },
+        {
+            code: `
+const a1 = \`foo\`
+const a2 = \`foo\${bar}baz\`
+const a3 = tag\`foo\`
+            `,
+            output: `
+const a1 = "foo"
+const a2 = "foo"+bar+"baz"
+const a3 = tag\`foo\`
+            `,
+            errors: [
+                "ES2015 template literals are forbidden.",
+                "ES2015 template literals are forbidden.",
+                "ES2015 template literals are forbidden.",
+            ],
+        },
+        {
+            //eslint-disable-next-line no-template-curly-in-string
+            code: "`${a}${b}`",
+            output: `""+a+b`,
+            errors: ["ES2015 template literals are forbidden."],
+        },
+        {
+            code: "``",
+            output: `""`,
+            errors: ["ES2015 template literals are forbidden."],
+        },
+        {
+            //eslint-disable-next-line no-template-curly-in-string
+            code: '`${""}`',
+            output: `""`,
+            errors: ["ES2015 template literals are forbidden."],
+        },
+        {
+            //eslint-disable-next-line no-template-curly-in-string
+            code: "`${''}`",
+            output: `''`,
+            errors: ["ES2015 template literals are forbidden."],
+        },
+        {
+            code: "`\n\t '\"\\`\\$\\${`",
+            output: `"\\n\\t '\\"\`$\${"`,
+            errors: ["ES2015 template literals are forbidden."],
+        },
+        {
+            //eslint-disable-next-line no-template-curly-in-string
+            code: "`${'abc'}`",
+            output: `'abc'`,
             errors: ["ES2015 template literals are forbidden."],
         },
     ],
