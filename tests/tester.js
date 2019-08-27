bd0d6708c2d473cecbc879821e3c08391285a1f2
Toru Nagashima <public@mysticatea.dev>
2019-08-27 22:45:26 +0900

    ⚒ switch CI to GitHub Actions (#17)

--- tests/tester.js
+++ tests/tester.js
@@ -4,15 +4,23 @@
  */
 "use strict"
 
-const { RuleTester } = require("eslint")
+const { Linter, RuleTester } = require("eslint")
 const { es2017 } = require("globals")
+const semver = require("semver")
+
+const eslintVersion = new Linter().version
+const ecmaVersion = semver.lt(eslintVersion, "5.0.0") ? 2018 : 2019
+
+//eslint-disable-next-line no-console
+console.log("ECMAScript Version: %d", ecmaVersion)
 
 RuleTester.setDefaultConfig({
     parserOptions: {
-        ecmaVersion: 2019,
+        ecmaVersion,
         sourceType: "script",
     },
     globals: es2017,
 })
+RuleTester.isSupported = targetEcmaVersion => targetEcmaVersion <= ecmaVersion
 
 module.exports = RuleTester
