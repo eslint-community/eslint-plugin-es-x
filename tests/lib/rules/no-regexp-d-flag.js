f4506eb8e2d228627231acccd4411207ef0fe6fb
yosuke ota <otameshiyo23@gmail.com>
2021-09-14 11:44:46 +0900

    update

--- tests/lib/rules/no-regexp-d-flag.js
+++ tests/lib/rules/no-regexp-d-flag.js
@@ -7,18 +7,13 @@
 const RuleTester = require("../../tester")
 const rule = require("../../../lib/rules/no-regexp-d-flag.js")
 
-// if (!RuleTester.isSupported(2022)) {
-//     //eslint-disable-next-line no-console
-//     console.log("Skip the tests of no-regexp-d-flag.")
-//     return
-// }
+if (!RuleTester.isSupported(2022)) {
+    //eslint-disable-next-line no-console
+    console.log("Skip the tests of no-regexp-d-flag.")
+    return
+}
 
-new RuleTester({
-    parser: require.resolve("espree"), // espree v8.0.0-beta.x
-    parserOptions: {
-        ecmaVersion: 2022,
-    },
-}).run("no-regexp-d-flag", rule, {
+new RuleTester().run("no-regexp-d-flag", rule, {
     valid: [
         "/foo/gimuys",
         "a\n/b/d",
