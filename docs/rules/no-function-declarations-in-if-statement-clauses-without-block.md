006037e48f252b6db38e4a48c9d4783a21559ca1
yosuke ota <otameshiyo23@gmail.com>
2022-03-14 17:32:15 +0900

    Support autofix

--- docs/rules/no-function-declarations-in-if-statement-clauses-without-block.md
+++ docs/rules/no-function-declarations-in-if-statement-clauses-without-block.md
@@ -1,6 +1,8 @@
 # es-x/no-function-declarations-in-if-statement-clauses-without-block
 > disallow function declarations in if statement clauses without using blocks
 
+- 🔧 The `--fix` option on the [command line](https://eslint.org/docs/user-guide/command-line-interface#fixing-problems) can automatically fix some of the problems reported by this rule.
+
 This rule reports Annex B feature [the function declarations in if statement clauses without using blocks](https://tc39.es/ecma262/multipage/additional-ecmascript-features-for-web-browsers.html#sec-functiondeclarations-in-ifstatement-statement-clauses) as errors.
 
 ## Examples
--- tests/lib/rules/no-function-declarations-in-if-statement-clauses-without-block.js
+++ tests/lib/rules/no-function-declarations-in-if-statement-clauses-without-block.js
@@ -18,13 +18,23 @@ new RuleTester({
     invalid: [
         {
             code: "if (a) function f() {}",
+            output: "if (a) {function f() {}}",
             errors: [
                 "Annex B feature the function declarations in if statement clauses without using blocks are forbidden.",
             ],
         },
         {
             code: "if (a); else function f() {}",
+            output: "if (a); else {function f() {}}",
             errors: [
+                "Annex B feature the function declarations in if statement clauses without using blocks are forbidden.",
+            ],
+        },
+        {
+            code: "if (a) function f1() {} else function f2() {}",
+            output: "if (a) {function f1() {}} else {function f2() {}}",
+            errors: [
+                "Annex B feature the function declarations in if statement clauses without using blocks are forbidden.",
                 "Annex B feature the function declarations in if statement clauses without using blocks are forbidden.",
             ],
         },
