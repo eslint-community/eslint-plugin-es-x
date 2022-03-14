64aedc6e7508b95355745027b1b8dcc6ec2b3762
yosuke ota <otameshiyo23@gmail.com>
2022-03-14 11:46:12 +0900

    Support for suggest

--- tests/lib/rules/no-string-prototype-trimleft-trimright.js
+++ tests/lib/rules/no-string-prototype-trimleft-trimright.js
@@ -22,10 +22,34 @@ const WITH_STRING = `
 'foo'.trimRight(2)
 `
 
+const FIX_FOR_WITH_ID_FOO = `
+foo.trimStart(2)
+foo.trimEnd(2)
+`
+const FIX_FOR_WITH_ID_A = `
+a.trimStart(2)
+a.trimEnd(2)
+`
+const FIX_FOR_WITH_STRING = `
+'foo'.trimStart(2)
+'foo'.trimEnd(2)
+`
+
 const FULL_ERRORS = [
     "Annex B feature 'String.prototype.trimLeft' method is forbidden.",
     "Annex B feature 'String.prototype.trimRight' method is forbidden.",
 ]
+
+function createSuggestions(code, errorIndex) {
+    const output =
+        errorIndex === 0
+            ? code.replace("trimLeft", "trimStart")
+            : code.replace("trimRight", "trimEnd")
+    const desc =
+        errorIndex === 0 ? "Replace with 'trimStart'" : "Replace with 'trimEnd'"
+
+    return [{ desc, output }]
+}
 
 new RuleTester().run(ruleId, rule, {
     valid: [
@@ -46,17 +70,26 @@ new RuleTester().run(ruleId, rule, {
     invalid: [
         {
             code: WITH_STRING,
+            output: FIX_FOR_WITH_STRING,
             errors: FULL_ERRORS,
         },
         {
             code: WITH_ID_FOO,
-            errors: FULL_ERRORS,
+            output: null,
+            errors: FULL_ERRORS.map((message, i) => ({
+                message,
+                suggestions: createSuggestions(WITH_ID_FOO, i),
+            })),
             settings: { "es-x": { aggressive: true } },
         },
         {
             code: WITH_ID_FOO,
+            output: null,
             options: [{ aggressive: true }],
-            errors: FULL_ERRORS,
+            errors: FULL_ERRORS.map((message, i) => ({
+                message,
+                suggestions: createSuggestions(WITH_ID_FOO, i),
+            })),
             settings: { "es-x": { aggressive: false } },
         },
     ],
@@ -94,33 +127,48 @@ new RuleTester({ parser }).run(`${ruleId} TS`, rule, {
         {
             filename,
             code: WITH_STRING,
+            output: FIX_FOR_WITH_STRING,
             errors: FULL_ERRORS,
         },
         {
             filename,
             code: `let foo = 'foo'; ${WITH_ID_FOO}`,
+            output: `let foo = 'foo'; ${FIX_FOR_WITH_ID_FOO}`,
             errors: FULL_ERRORS,
         },
         {
             filename,
             code: `function f<T extends string>(a: T) { ${WITH_ID_A} }`,
+            output: `function f<T extends string>(a: T) { ${FIX_FOR_WITH_ID_A} }`,
             errors: FULL_ERRORS,
         },
         {
             filename,
             code: `function f<T extends 'a' | 'b'>(a: T) { ${WITH_ID_A} }`,
+            output: `function f<T extends 'a' | 'b'>(a: T) { ${FIX_FOR_WITH_ID_A} }`,
             errors: FULL_ERRORS,
         },
         {
             filename,
             code: `let foo = String(); ${WITH_ID_FOO}`,
-            errors: FULL_ERRORS,
+            output: null,
+            errors: FULL_ERRORS.map((message, i) => ({
+                message,
+                suggestions: createSuggestions(
+                    `let foo = String(); ${WITH_ID_FOO}`,
+                    i,
+                ),
+            })),
             settings: { "es-x": { aggressive: true } },
         },
         {
             filename,
             code: WITH_ID_FOO,
-            errors: FULL_ERRORS,
+            output: null,
+            errors: FULL_ERRORS.map((message, i) => ({
+                message,
+                suggestions: createSuggestions(WITH_ID_FOO, i),
+            })),
             settings: { "es-x": { aggressive: true } },
         },
     ],
@@ -150,32 +198,41 @@ new RuleTester({ parser, parserOptions: { tsconfigRootDir, project } }).run(
             {
                 filename,
                 code: WITH_STRING,
+                output: FIX_FOR_WITH_STRING,
                 errors: FULL_ERRORS,
             },
             {
                 filename,
                 code: `let foo = 'foo'; ${WITH_ID_FOO}`,
+                output: `let foo = 'foo'; ${FIX_FOR_WITH_ID_FOO}`,
                 errors: FULL_ERRORS,
             },
             {
                 filename,
                 code: `let foo = String(); ${WITH_ID_FOO}`,
+                output: `let foo = String(); ${FIX_FOR_WITH_ID_FOO}`,
                 errors: FULL_ERRORS,
             },
             {
                 filename,
                 code: `function f<T extends string>(a: T) { ${WITH_ID_A} }`,
+                output: `function f<T extends string>(a: T) { ${FIX_FOR_WITH_ID_A} }`,
                 errors: FULL_ERRORS,
             },
             {
                 filename,
                 code: `function f<T extends 'a' | 'b'>(a: T) {${WITH_ID_A} }`,
+                output: `function f<T extends 'a' | 'b'>(a: T) {${FIX_FOR_WITH_ID_A} }`,
                 errors: FULL_ERRORS,
             },
             {
                 filename,
                 code: WITH_ID_FOO,
-                errors: FULL_ERRORS,
+                output: null,
+                errors: FULL_ERRORS.map((message, i) => ({
+                    message,
+                    suggestions: createSuggestions(WITH_ID_FOO, i),
+                })),
                 settings: { "es-x": { aggressive: true } },
             },
         ],
