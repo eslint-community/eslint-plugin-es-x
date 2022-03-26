a801193458ece7c6d411abcc4b6d6b58b4f61537
Sosuke Suzuki <aosukeke@gmail.com>
2022-03-27 01:26:56 +0900

    Support more error constructors

--- tests/lib/rules/no-error-cause.js
+++ tests/lib/rules/no-error-cause.js
@@ -14,55 +14,51 @@ if (!RuleTester.isSupported(2022)) {
     return
 }
 
+const errorConstructorNames = [
+    "Error",
+    "AggregateError",
+    "EvalError",
+    "RangeError",
+    "ReferenceError",
+    "SyntaxError",
+    "TypeError",
+    "URIError",
+]
+
+const valid = errorConstructorNames
+    .map((errorConstructorName) => [
+        `new ${errorConstructorName}("message")`,
+        `new ${errorConstructorName}("message", notObjectExpression)`,
+        `new ${errorConstructorName}("message", { notCause: foo })`,
+        `class MyError extends ${errorConstructorName} { constructor() { super("message") } }`,
+        `class MyError extends ${errorConstructorName} { constructor() { super("message", notObjectExpression) } }`,
+        `class MyError extends ${errorConstructorName} { constructor() { super("message", { notCause: foo }) } }`,
+    ])
+    // alternative of `Array.prototype.flat`
+    .reduce((acc, val) => acc.concat(val), [])
+
+const invalid = errorConstructorNames
+    .map((errorConstructorName) => [
+        {
+            code: `new ${errorConstructorName}("message", { cause: foo });`,
+            errors: ["ES2022 Error Cause is forbidden."],
+        },
+        {
+            code: `new ${errorConstructorName}("message", { ["cause"]: foo });`,
+            errors: ["ES2022 Error Cause is forbidden."],
+        },
+        {
+            code: `const MyError = ${errorConstructorName}; new MyError("message", { ["cause"]: foo });`,
+            errors: ["ES2022 Error Cause is forbidden."],
+        },
+        {
+            code: `class MyError extends ${errorConstructorName} { constructor() { super("message", { cause: foo }); } }`,
+            errors: ["ES2022 Error Cause is forbidden."],
+        },
+    ])
+    // alternative of `Array.prototype.flat`
+    .reduce((acc, val) => acc.concat(val), [])
+
 new RuleTester({
     parserOptions: { sourceType: "module" },
-}).run("no-error-cause", rule, {
-    valid: [
-        'new Error("message")',
-        'new Error("message", notObjectExpression)',
-        'new Error("message", { notCause: foo })',
-        'class MyError extends Error { constructor() { super("message") } }',
-        'class MyError extends Error { constructor() { super("message", notObjectExpression) } }',
-        'class MyError extends Error { constructor() { super("message", { notCause: foo }) } }',
-        'new AggregateError("message")',
-        'new AggregateError("message", notObjectExpression)',
-        'new AggregateError("message", { notCause: foo })',
-        'class MyError extends AggregateError { constructor() { super("message") } }',
-        'class MyError extends AggregateError { constructor() { super("message", notObjectExpression) } }',
-        'class MyError extends AggregateError { constructor() { super("message", { notCause: foo }) } }',
-    ],
-    invalid: [
-        {
-            code: 'new Error("message", { cause: foo });',
-            errors: ["ES2022 Error Cause is forbidden."],
-        },
-        {
-            code: 'new Error("message", { ["cause"]: foo });',
-            errors: ["ES2022 Error Cause is forbidden."],
-        },
-        {
-            code: 'const MyError = Error; new MyError("message", { ["cause"]: foo });',
-            errors: ["ES2022 Error Cause is forbidden."],
-        },
-        {
-            code: 'class MyError extends Error { constructor() { super("message", { cause: foo }); } }',
-            errors: ["ES2022 Error Cause is forbidden."],
-        },
-        {
-            code: 'new AggregateError("message", { cause: foo });',
-            errors: ["ES2022 Error Cause is forbidden."],
-        },
-        {
-            code: 'new AggregateError("message", { ["cause"]: foo });',
-            errors: ["ES2022 Error Cause is forbidden."],
-        },
-        {
-            code: 'const MyError = AggregateError; new MyError("message", { ["cause"]: foo });',
-            errors: ["ES2022 Error Cause is forbidden."],
-        },
-        {
-            code: 'class MyError extends AggregateError { constructor() { super("message", { cause: foo }); } }',
-            errors: ["ES2022 Error Cause is forbidden."],
-        },
-    ],
-})
+}).run("no-error-cause", rule, { valid, invalid })
