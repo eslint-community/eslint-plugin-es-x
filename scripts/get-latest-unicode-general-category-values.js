fc8363fea0d35f9da2eaf5e1924c38beb2794689
yosuke ota <otameshiyo23@gmail.com>
2023-03-15 15:34:12 +0900

    refactor

--- scripts/get-latest-unicode-general-category-values.js
+++ scripts/get-latest-unicode-general-category-values.js
@@ -1,29 +1,17 @@
 "use strict"
 
-const { fetchLines } = require("./fetch-lines")
-
-const DB_URL =
-    "https://unicode.org/Public/UCD/latest/ucd/PropertyValueAliases.txt"
-const logger = console
+const {
+    getLatestUnicodePropertyValues,
+} = require("./get-latest-unicode-property-values")
 
 module.exports = { getLatestUnicodeGeneralCategoryValues }
 
 async function* getLatestUnicodeGeneralCategoryValues() {
-    logger.log("Fetching data... (%s)", DB_URL)
-    for await (const line of fetchLines(DB_URL)) {
-        if (!line || line.startsWith("#")) {
-            continue
-        }
-        const [propertyAlias, alias, canonical, ...remaining] = line
-            .split("#")[0] // strip comments
-            .split(";") // split by semicolon
-            .map((x) => x.trim()) // trim
-        if (propertyAlias !== "gc") {
+    for await (const value of getLatestUnicodePropertyValues()) {
+        if (value.propertyAlias !== "gc") {
             continue
         }
 
-        yield alias
-        yield canonical
-        yield* remaining
+        yield* value.aliases
     }
 }
