9fb12d0df8e8562c373748ac0f47187a129e198a
yosuke ota <otameshiyo23@gmail.com>
2023-03-23 09:22:00 +0900

    chore: use node-fetch

--- scripts/fetch-lines.js
+++ scripts/fetch-lines.js
@@ -1,68 +1,29 @@
 "use strict"
 
-const https = require("https")
-
 module.exports = { fetchLines }
 
-function fetchLines(url) {
-    const linesBuffer = []
-
-    let resolve = undefined
-    let reject = undefined
-
-    function pushNext() {
-        linesBuffer.push(
-            new Promise((res, rej) => {
-                resolve = res
-                reject = rej
-            }),
-        )
-    }
-
-    function resolveLine(line) {
-        resolve({ value: line })
-        pushNext()
-    }
-
-    pushNext()
-
-    const itr = {
-        [Symbol.asyncIterator]() {
-            return {
-                next() {
-                    return linesBuffer.shift()
-                },
+async function* fetchLines(url) {
+    const response = await fetch(url)
+    const reader = response.body.getReader()
+    let buffer = ""
+    const decoder = new TextDecoder()
+    while (true) {
+        const { done, value: chunk } = await reader.read()
+        if (done) {
+            if (chunk) {
+                buffer += decoder.decode(chunk)
             }
-        },
+            yield buffer
+            break
+        }
+        const lines = (buffer + decoder.decode(chunk)).split("\n")
+        if (lines.length === 1) {
+            buffer = lines[0]
+        } else {
+            buffer = lines.pop()
+            for (const line of lines) {
+                yield line
+            }
+        }
     }
-    https
-        .get(url, (res) => {
-            let buffer = ""
-            res.setEncoding("utf8")
-            res.on("data", (chunk) => {
-                const lines = (buffer + String(chunk)).split("\n")
-                if (lines.length === 1) {
-                    buffer = lines[0]
-                } else {
-                    buffer = lines.pop()
-                    for (const line of lines) {
-                        resolveLine(line)
-                    }
-                }
-            })
-            res.on("end", () => {
-                if (buffer) {
-                    resolveLine(buffer)
-                }
-                resolve({ done: true, value: undefined })
-            })
-            res.on("error", (e) => {
-                reject(e)
-            })
-        })
-        .on("error", (e) => {
-            reject(e)
-        })
-
-    return itr
 }
