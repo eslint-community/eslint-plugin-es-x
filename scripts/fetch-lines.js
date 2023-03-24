f038af7162107b9e6436603a201234556e3131f2
ota-meshi <otameshiyo23@gmail.com>
2023-03-24 13:08:46 +0900

    chore: use fetch().text()

--- scripts/fetch-lines.js
+++ scripts/fetch-lines.js
@@ -4,18 +4,5 @@ module.exports = { fetchLines }
 
 async function* fetchLines(url) {
     const response = await fetch(url)
-    let buffer = ""
-    const decoder = new TextDecoder()
-    for await (const chunk of response.body) {
-        const lines = (buffer + decoder.decode(chunk)).split("\n")
-        if (lines.length === 1) {
-            buffer = lines[0]
-        } else {
-            buffer = lines.pop()
-            for (const line of lines) {
-                yield line
-            }
-        }
-    }
-    yield buffer
+    yield* (await response.text()).split("\n")
 }
