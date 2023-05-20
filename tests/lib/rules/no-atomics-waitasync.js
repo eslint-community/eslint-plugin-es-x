0d783bdce3438ffc3e0afacea3f3c9a2e812b8c8
SUZUKI Sosuke <sosuke.suzuki@dr-ubie.com>
2023-05-21 08:37:48 +0900

    Add new rule `no-atomics-waitasync` (#75)
    Co-authored-by: Yosuke Ota <otameshiyo23@gmail.com>

---
+++ tests/lib/rules/no-atomics-waitasync.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const RuleTester = require("../../tester")
+const rule = require("../../../lib/rules/no-atomics-waitasync.js")
+
+new RuleTester().run("no-atomics-waitasync", rule, {
+    valid: ["Atomics", "Atomics.wait", "let Atomics = 0; Atomics.watiAsync"],
+    invalid: [
+        {
+            code: "Atomics.waitAsync",
+            errors: ["ES2024 'Atomics.waitAsync' method is forbidden."],
+        },
+    ],
+})
