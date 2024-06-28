81db34c3e75271af5a7940b3ea6cdeed803a5abd
yosuke ota <otameshiyo23@gmail.com>
2024-06-28 17:09:42 +0900

    update deps

--- tests/tester.js
+++ tests/tester.js
@@ -5,6 +5,7 @@
 "use strict"
 
 const { Linter } = require("eslint")
+const espree = require("espree")
 const { getRuleTester } = require("eslint-compat-utils/rule-tester")
 const { builtin } = require("globals")
 const semver = require("semver")
@@ -13,7 +14,7 @@ const RuleTester = getRuleTester()
 const eslintVersion = new Linter().version
 const ecmaVersion =
     /*eslint-disable prettier/prettier */
-    semver.gte(eslintVersion, "8.44.0") ? 2024 :
+    semver.gte(eslintVersion, "8.44.0") ? espree.latestEcmaVersion + 2009 :
     semver.gte(eslintVersion, "8.23.0") ? 2023 :
     semver.gte(eslintVersion, "8.0.0") ? 2022 :
     semver.gte(eslintVersion, "7.8.0") ? 2021 :
