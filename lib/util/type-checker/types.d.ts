12fc0f371dd5fb44fd8c26cdc3dfdfa38526ca16
yosuke ota <otameshiyo23@gmail.com>
2025-04-04 22:34:25 +0900

    Add `no-atomics-pause` rule

--- lib/util/type-checker/types.d.ts
+++ lib/util/type-checker/types.d.ts
@@ -234,3 +234,6 @@ export type MathProperty = Exclude<keyof typeof Math, ExcludeProperty>;
 export type ErrorProperty =
   | Exclude<keyof typeof Error, ExcludeProperty | "prepareStackTrace">
   | "isError";
+export type AtomicsProperty =
+  | Exclude<keyof typeof Atomics, ExcludeProperty>
+  | "pause";
