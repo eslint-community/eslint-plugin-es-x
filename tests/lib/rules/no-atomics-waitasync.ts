65620617a41a9352537c5c822292ab574c6f3777
yosuke ota <otameshiyo23@gmail.com>
2025-04-04 22:34:25 +0900

    Add `no-atomics-pause` rule

--- tests/lib/rules/no-atomics-waitasync.ts
+++ tests/lib/rules/no-atomics-waitasync.ts
@@ -2,7 +2,7 @@ import RuleTester from "../../tester"
 import * as rule from "../../../lib/rules/no-atomics-waitasync"
 
 new RuleTester().run("no-atomics-waitasync", rule, {
-    valid: ["Atomics", "Atomics.wait", "let Atomics = 0; Atomics.watiAsync"],
+    valid: ["Atomics", "Atomics.wait", "let Atomics = 0; Atomics.waitAsync"],
     invalid: [
         {
             code: "Atomics.waitAsync",
