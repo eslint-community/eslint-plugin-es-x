ee45bf52a2e68a41ede50027940d9fb6b09f73d4
Jason Weinzierl <weinzierljason@gmail.com>
2026-03-12 23:36:52 -0500

    refactor: wrap each rule in createRule with type inference (#335)

---
+++ lib/util/create-rule.d.ts
@@ -0,0 +1,35 @@
+import type { RuleDefinition } from "@eslint/core"
+import type { JSSyntaxElement, Linter, Rule, SourceCode } from "eslint"
+
+interface EsXRuleDocs<Desc extends string> {
+    description: Desc
+    category?: string
+    proposal?: string
+    recommended: boolean
+    url: string
+}
+
+type EsXRuleModule<
+    RuleOptions extends unknown[],
+    MessageIds extends string,
+    Docs extends EsXRuleDocs<string>,
+> = RuleDefinition<{
+    LangOptions: Linter.LanguageOptions
+    Code: SourceCode
+    RuleOptions: RuleOptions
+    Visitor: Rule.RuleListener
+    Node: JSSyntaxElement
+    MessageIds: MessageIds
+    ExtRuleDocs: Docs
+}>
+
+/**
+ * Define the rule.
+ * @param rule rule module
+ */
+export function createRule<
+    MessageIds extends string,
+    Desc extends string,
+    Docs extends EsXRuleDocs<Desc>,
+    RuleOptions extends unknown[] = any[],
+>(rule: EsXRuleModule<RuleOptions, MessageIds, Docs>): EsXRuleModule<RuleOptions, MessageIds, Docs>;
